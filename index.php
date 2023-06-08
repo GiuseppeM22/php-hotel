3c8c32737d6b1d5a8fdbab7c34df73c5530529af
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-06-08 16:14:11 +0200

    add :table

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <title>Hotel</title>
 </head>
 <body>
@@ -53,12 +54,36 @@
 
         ?>
 
-        <?php foreach($hotels as $hotel) { ?>
-          
-            <div> <?php echo $hotel['name'] ?> </div>
-        
-        <?php } ?>
-     
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Center Distance</th>
+    </tr>
+  </thead>
+
+  <tbody>
+      <?php foreach($hotels as $hotel) { ?>
+       <?php $park = $hotel["parking"] ? '<i class="fa-solid fa-square-parking" style="color: #00ff04;"></i>' : '<i class="fa-solid fa-square-parking" style="color: #ff0000;"></i>' ?>
+    <tr>
+      <td><?php echo $hotel['name'] ?></td>
+      <td><?php echo $hotel['description'] ?></td>
+      <td><?php echo $park ?></td>
+      <td><?php echo $hotel['vote'] ?></td>
+      <td><?php echo $hotel['distance_to_center'] ?></td>
+
+    </tr>
+    
+    
+    <?php } ?>
+    
+
+  </tbody>
+</table>
+
         
        
         
